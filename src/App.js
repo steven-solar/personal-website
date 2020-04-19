420cc8c4e15baeef0f2280c52995c0d4a5185d4c
ssolar <steven.solar@grantsolutions.gov>
2020-04-19 13:56:53 -0400

    init, routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Route, 
+  Link, 
+  BrowserRouter as Router
+} from 'react-router-dom';
+import Home from './Home.js';
+import About from './About.js';
+import Skills from './Skills.js';
+import Projects from './Projects.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li> <Link to='/'> Home </Link> </li>
+            <li> <Link to='/about'> About </Link> </li>
+            <li> <Link to='/skills'> Skills </Link> </li>
+            <li> <Link to='/projects'> Projects </Link> </li>
+          </ul>
+        </div>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/skills' component={Skills} />
+        <Route path='/projects' component={Projects} />
+      </Router>
+
+
+    );
+  }
 }
 
 export default App;
