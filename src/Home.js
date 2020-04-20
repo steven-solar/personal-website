150527363e3109162796ee453e2db8fa42116c00
ssolar <steven.solar@grantsolutions.gov>
2020-04-19 22:21:31 -0400

    skills done (for now)

--- src/Home.js
+++ src/Home.js
@@ -12,11 +12,13 @@ class Home extends Component {
             <div className="container home-container"> 
                 <div className="row justify-content-md-center name"> Steven Solar </div>
                 <div className="row justify-content-md-center descrip"> &lt;developer, engineer, enthusiast&#47;&gt; </div>
-                <div> 
-                    <a href="https://github.com/steven-solar" target="_blank"> <img src="./images/github.png"/> </a>
-                    <a href="https://www.linkedin.com/in/steven-solar" target="_blank"> <img src="./images/linkedin.png" /> </a>
-                    <Link to="/resume" target="_blank"> <img src="./resume.png" /> </Link>
+                <div className="row justify-content-md-center"> 
+                    <a className="logos" href="https://github.com/steven-solar" target="_blank"> <img height="75" src="https://cdn.iconscout.com/icon/free/png-256/github-153-675523.png"/> </a>
+                    <a className="logos" href="https://www.linkedin.com/in/steven-solar" target="_blank"> <img height="75" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png" /> </a>
+                    <Link className="logos" to="/resume" target="_blank"> <img height="75" src="https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-resume-icon-png-image_4260240.jpg" /> </Link>
                 </div>
+                <div> Experience </div>
+                
                 
             </div>   
         );
--- src/Skills.js
+++ src/Skills.js
@@ -5,28 +5,29 @@ class Skills extends Component {
     render() {
         return (
             <div className="skills-container"> 
-
-                <div className="descrip"> Frontend Development </div>
+                <div className="name"> Skills </div>
+                <div className="descrip"> I love learning and building! Here is some of what I've picked up along the way!</div>
+                <div className="skill-category"> Frontend Development </div>
                 <span className="skill-item"> Javascript </span>
                 <span className="skill-item"> React </span>
                 <span className="skill-item"> Redux </span>
                 <span className="skill-item"> HTML </span>
                 <span className="skill-item"> CSS </span>
 
-                <div className="descrip"> Backend Development </div>
+                <div className="skill-category"> Backend Development </div>
                 <span className="skill-item"> Python </span>
                 <span className="skill-item"> Django </span>
                 <span className="skill-item"> Node.js </span>
                 <span className="skill-item"> Express </span> 
                 <span className="skill-item"> SQL </span> 
 
-                <div className="descrip"> General Languages </div>
+                <div className="skill-category"> General Languages </div>
                 <span className="skill-item"> Python </span>  
                 <span className="skill-item"> Java </span>
                 <span className="skill-item"> C </span>
                 <span className="skill-item"> C++ </span>
                     
-                <div className="descrip"> Coursework </div>
+                <div className="skill-category"> Coursework </div>
                 <span className="skill-item"> Data Structures </span>  
                 <span className="skill-item"> Algorithms </span>
                 <span className="skill-item"> Computer Networks </span>
@@ -34,7 +35,7 @@ class Skills extends Component {
                 <span className="skill-item"> Probability &amp; Statistics </span>
                 <span className="skill-item"> Calculus I-III </span>
 
-                <div className="descrip"> Tools </div>
+                <div className="skill-category"> Tools </div>
                 <span className="skill-item"> Git </span>
                 <span className="skill-item"> Sketch </span>  
                 <span className="skill-item"> Zeplin </span>
